d604937a6663281dd9ed495d8c6079344530148b
MarceloX2115 <marcelosampaio827@gmail.com>
2026-05-26 18:18:40 -0300

    Adicionando o delete

--- front/js/delete.js
+++ front/js/delete.js
@@ -1,1 +1,11 @@
-fetch('')
+async function excluirHeroi(id, nome) {
+  try {
+    const res = await fetch('https://heroes-ml-views.up.railway.app/api/herois/22', { method: 'DELETE' });
+    if (!res.ok) throw new Error();
+    console.log('Herói excluído.', 'success');
+  } catch {
+    console.log('Erro ao excluir herói.', 'error');
+  }
+}
+
+excluirHeroi()
